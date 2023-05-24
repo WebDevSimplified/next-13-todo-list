0f7fa9c72ff4de70b342e912982ec404d3137990
Hillary Muboka <91418871+Laryd@users.noreply.github.com>
2023-05-24 04:55:01 +0300

    Update page.tsx
    imported toggleTodo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
 import { TodoItem } from "@/components/TodoItem"
+import {toggleTodo} from "@/components/ServerSideComponents
 import { prisma } from "@/db"
 import Link from "next/link"
 
 function getTodos() {
   return prisma.todo.findMany()
-}
-
-async function toggleTodo(id: string, complete: boolean) {
-  "use server"
-
-  await prisma.todo.update({ where: { id }, data: { complete } })
 }
 
 export default async function Home() {
