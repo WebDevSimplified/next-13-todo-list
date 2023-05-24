497c9e766d0128409b583dd960ebdee33e5d2cf0
Hillary Muboka <91418871+Laryd@users.noreply.github.com>
2023-05-24 04:56:48 +0300

    Update page.tsx
    createTodo import

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,18 +1,7 @@
-import { prisma } from "@/db"
-import { redirect } from "next/navigation"
+
+import { createTodo } from "@/components/ServerSideComponents
 import Link from "next/link"
 
-async function createTodo(data: FormData) {
-  "use server"
-
-  const title = data.get("title")?.valueOf()
-  if (typeof title !== "string" || title.length === 0) {
-    throw new Error("Invalid Title")
-  }
-
-  await prisma.todo.create({ data: { title, complete: false } })
-  redirect("/")
-}
 
 export default function Page() {
   return (
